b0543548d8724bbce36497080abd37acdfd949e3
AcedYu <alexyu299@hotmail.com>
2021-04-13 21:32:27 -0700

    inquirer set up

--- index.js
+++ index.js
@@ -3,15 +3,52 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-  'What is your GitHub username?',
-  'What is your email address?',
-  "What is your project's name?",
-  "Please write a short description of your project",
-  "What kind of license should your project have?",
-  "What command should be run to install dependencies?",
-  "What command should be run to run tests?",
-  "What does the user need to know about using the repo?",
-  "What does the user need to know about contributing to the repo?"
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: "github"
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: "email"
+  },
+  {
+    type: 'input',
+    message: "What is your project's name?",
+    name: 'project'
+  },
+  {
+    type: 'input',
+    message: 'Please write a short description of your project',
+    name: 'description'
+  },
+  {
+    type: 'list',
+    message: "What kind of license should your project have?",
+    name: 'license',
+    choices: []
+  },
+  {
+    type: 'input',
+    message: "What command should be run to install dependencies?",
+    name: 'dependencies'
+  },
+  {
+    type: 'input',
+    message: "What command should be run to run tests?",
+    name: 'tests'
+  },
+  {
+    type: 'input',
+    message: "What does the user need to know about using the repo?",
+    name: 'needtoknow'
+  },
+  {
+    type: 'input',
+    message: "What does the user need to know about contributing to the repo?",
+    name: "conbribute"
+  }
 ];
 
 // TODO: Create a function to write README file
@@ -20,11 +57,10 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
   inquirer
-    .prompt([
-
-    ])
+    .prompt(questions)
     .then((response) => {
-
+      var file = `${response.project.toLowerCase().split(' ').join('')}_README.md`;
+      writeToFile(file, response);
     });
 }
 
